814ec7f55792fb097db5a3159e871ca594532adb
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-05 11:40:12 +0600

    ListView.separeted is added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AppBar"),
+        title: Text("CRUD"),
+      ),
+      body: ListView.separated(
+        itemCount: 20,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: Icon(Icons.image),
+              title: Text("Product Name"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Product Code"),
+                  Text("Total Price"),
+                  Text("Available Units"),
+                ],
+              ),
+              trailing: Text("Unit Price"),
+            );
+          }, separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            color: Colors.red,
+            height: 1,
+          );
+      },
       ),
     );
   }
